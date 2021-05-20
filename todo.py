32f8c2ea16a395b6ae906abbec6206d53faacd5a
ahsan <ahsanzakpri@gmail.com>
2021-05-20 18:27:48 +0530

    Used SQLAlchemy and Marshmallow

--- todo.py
+++ todo.py
@@ -1,80 +1,81 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+import os
 import json
 import sqlite3
 
+# Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    status = db.Column(db.String(50))
 
-def db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect("todos.sqlite")
-    except sqlite3.error as e:
-        print(e)
-    return conn
+    def __init__(self, title, status):
+        self.title = title
+        self.status = status
 
+class TodoSchema(ma.Schema):
+    class meta:
+        fields = ('id', 'title', 'status')
+
+# Init Schema 
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
 
 @app.route("/todos", methods=["GET", "POST"])
 def todos():
-    conn = db_connection()
-    cursor = conn.cursor()
-
     if request.method == "GET":
-        cursor = conn.execute("SELECT * FROM todo")
-        todos = [
-            dict(id=row[0], title=row[1], status=row[2])
-            for row in cursor.fetchall()
-        ]
-        if todos is not None:
-            return jsonify(todos)
+        all_todos = Todo.query.all()
+        result = todos_schema.dump(all_todos)
+        return jsonify(result)
 
     if request.method == "POST":
-        new_title = request.form["title"]
-        new_status = request.form["status"]
-        sql = """INSERT INTO todo (title, status)
-                 VALUES (?, ?)"""
-        cursor = cursor.execute(sql, (new_title, new_status))
-        conn.commit()
-        return f"Todo with the id: {cursor.lastrowid} created successfully", 201
+        title = request.json['title']
+        status = request.json['status']
+        new_todo = Todo(title, status)
+        db.session.add(new_todo)
+        db.session.commit()
+        return todo_schema.jsonify(new_todo)
+        
 
 
 @app.route("/todo/<int:id>", methods=["GET", "PUT", "DELETE"])
 def single_todo(id):
-    conn = db_connection()
-    cursor = conn.cursor()
-    todo = None
     if request.method == "GET":
-        cursor.execute("SELECT * FROM todo WHERE id=?", (id,))
-        rows = cursor.fetchall()
-        for r in rows:
-            todo = r
-        if todo is not None:
-            return jsonify(todo), 200
-        else:
-            return "Something wrong", 404
+        todo = Todo.query.get(id)
+        return todo_schema.jsonify(todo)
 
     if request.method == "PUT":
-        sql = """UPDATE todo
-                SET title=?,
-                    status=?,
-                WHERE id=? """
+        todo = Todo.query.get(id)
 
-        title = request.form["title"]
-        status = request.form["status"]
-        updated_todo = {
-            "id": id,
-            "title": title,
-            "status": status,
-        }
-        conn.execute(sql, (title, status, id))
-        conn.commit()
-        return jsonify(updated_todo)
+        title = request.json['title']
+        status = request.json['status']
+
+        todo.title = title
+        todo.status = status
+ 
+        db.session.commit()
+
+        return todo_schema.jsonify(todo)
 
     if request.method == "DELETE":
-        sql = """ DELETE FROM todo WHERE id=? """
-        conn.execute(sql, (id,))
-        conn.commit()
-        return "The todo with id: {} has been deleted.".format(id), 200
+        todo = Todo.query.get(id)
+        db.session.delete(todo)
+        db.session.commit()
+
+        return todo_schema.jsonify(todo)
 
 
 if __name__ == "__main__":
