c61dfd01f63853c86fb6d4acfe558cc8fdfab647
ahsan <ahsanzakpri@gmail.com>
2021-05-24 11:32:05 +0530

    Registered a Blueprint for Admin Side

---
+++ admin/admin.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+from flask import Flask
+
+app = Flask(__name__)
+dashboard = Blueprint("dashboard", __name__, static_folder="static", template_folder="templates")
+
+@dashboard.route("/admin")
+@dashboard.route("/")
+def adminpanel():
+    return render_template("index.html")
--- todo/__init__.py
+++ todo/__init__.py
@@ -3,8 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 import os
 
+# Importing Blueprints
+from admin.admin import dashboard
+
 # Init app
 app = Flask(__name__)
+app.register_blueprint(dashboard, url_prefix="/admin")
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
