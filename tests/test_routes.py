2bc66d6a4e5164e11ba3c41fb54b611f68092690
ahsan <ahsanzakpri@gmail.com>
2021-05-29 12:35:49 +0530

    Pytesting done for CRUD

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
 import json
-from todo.routes import configure_routes
+# from todo.routes import configure_routes
 from todo.models import TodoSchema, Todo, todo_schema, todos_schema
+import unittest
+from run import app
 
 def test_base_route():
-    app = Flask(__name__)
-    configure_routes(app)
+    # app = Flask(__name__)
+    # configure_routes(app)
     client = app.test_client()
     url='/'
 
@@ -13,44 +15,53 @@ def test_base_route():
     assert response.get_data() == b"<h2>This is home </h2>"
     assert response.status_code == 200
 
+#Check POST todo
+def test_post_route__success():
+    # app = Flask(__name__)
+    # configure_routes(app)
+    client = app.test_client()
+    url = '/todos'
 
-# def test_post_route__success():
-#     app = Flask(__name__)
-#     configure_routes(app)
-#     client = app.test_client()
-#     url = '/todos'
-    
-#     all_todos = Todo.query.all()
-#     result = todos_schema.dump(all_todos)
-    
-#     mock_request_headers = {
-#         'ContentType': 'application/json',
-#         'dataType': 'json'
-#     }
-
-#     mock_request_data = result
-
-#     response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
-#     assert response.status_code == 200
-
-# def test_get_todos_check_content_type_equals_json():
-#     app = Flask(__name__)
-#     configure_routes(app)
-#     client = app.test_client()
-#     url='/todos'
-
-#     response = client.get(url)
-#     assert response.content_type == 'application/json'
-#     assert response.status_code == 200
+    response = client.post(url, json={"title":"newe title", "status": "Not comopleted"})
+    print(response)
+    assert response.status_code == 200
 
 
+#Check if Data returned
+def test_todos_data():
+    client = app.test_client()
+    response = client.get("/todo")
+    assert (b'title' in response.data)
+
+#Check Content Type 
+def test_get_todos_check_content_type_equals_json():
+    client = app.test_client()
+    response = client.get("/todo")
+    assert response.content_type == 'application/json'
+    assert response.status_code == 200
+
+# Check GET todo
 def test_todo_by_id():
-    app = Flask(__name__)
-    configure_routes(app)
     client = app.test_client()
     url='/todo/{}'
-    # For 200 Status
-    response = client.get(url.format(test_todo_by_id))
+    response = client.get(url.format(2))
     data = json.loads(response.data)
-    assert response.status_code == HTTPStatus.OK
-    # assert data['title']['status'] == ''
+    assert response.status_code == 200
+    assert data['title'] == 'This is the second Task to do'
+
+#Check PUT todo
+def test_put_todos():
+    client = app.test_client()
+    url='/todo/{}'
+    response = client.put(url.format(1), json={"title":"Updatedd", "status": "completed"})
+    assert response.status_code == 200
+
+
+#Check DELETE todo
+def test_put_todos():
+    client = app.test_client()
+    url='/todo/{}'
+    response = client.delete(url.format(8))
+    assert response.status_code == 200
+
+
